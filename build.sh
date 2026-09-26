d732fa6c37472002b82d3095a7f60e615479f4b4
ADITYA ARYA <RengokuSensei@users.noreply.github.com>
2026-09-26 06:06:49 +0530

    fix(build): align isee.ko loading after mcDrvModule to resolve TEEC symbols for gz_tz_system

--- build.sh
+++ build.sh
@@ -489,20 +489,13 @@ for line in lines:
         add = [d for d in extra if d not in deps.split()]
         if add:
             line = line.rstrip('\n') + (' ' if deps else '') + ' '.join(add) + '\n'
-    # trusted_mem.ko must depend on mcDrvModule.ko (MobiCore), never isee.ko (stock parity)
-    if line.startswith('trusted_mem.ko:'):
-        deps = [d for d in line.rstrip('\n').split(':', 1)[1].split() if d != 'isee.ko']
-        if 'mcDrvModule.ko' not in deps:
-            deps = ['mcDrvModule.ko'] + deps
-        line = 'trusted_mem.ko: ' + ' '.join(deps) + '\n'
-    # gz_tz_system.ko must not depend on isee.ko
-    if line.startswith('gz_tz_system.ko:'):
-        deps = [d for d in line.rstrip('\n').split(':', 1)[1].split() if d != 'isee.ko']
-        line = 'gz_tz_system.ko: ' + ' '.join(deps) + '\n'
-    # iommu_gz.ko must not depend on isee.ko
-    if line.startswith('iommu_gz.ko:'):
-        deps = [d for d in line.rstrip('\n').split(':', 1)[1].split() if d != 'isee.ko']
-        line = 'iommu_gz.ko: ' + ' '.join(deps) + '\n'
+    # isee.ko should load after mcDrvModule.ko (stock alignment line 87/104)
+    if line.startswith('isee.ko:'):
+        deps = line.rstrip('\n').split(':', 1)[1]
+        extra = ['mcDrvModule.ko']
+        add = [d for d in extra if d not in deps.split()]
+        if add:
+            line = line.rstrip('\n') + (' ' if deps else '') + ' '.join(add) + '\n'
     out.append(line)
 with open(dep_path, 'w') as f:
     f.writelines(out)
@@ -531,9 +524,6 @@ while remaining:
         ready = sorted(remaining)
     order += ready
     remaining -= set(ready)
-
-# isee.ko must NOT be loaded in first-stage init (stock loads in second-stage)
-order = [m for m in order if m != 'isee']
 
 sys.stdout.write('\n'.join(order) + '\n')
 PYEOF
